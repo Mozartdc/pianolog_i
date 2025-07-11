98b6758798bdb927ee16206848cbdff0cf06f405
Mozart <dcdigitalpiano@gmail.com>
2025-07-11 11:40:34 +0900

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -621,7 +621,7 @@ function HomeScreen() {
       <Header 
         title="digital piano gallery 피출앱"
         color="var(--TURQUOISE)" // ✅ CSS 변수 적용
-        topMargin={10}
+        topMargin={20}
       />
 
       {/* Date Display */}
