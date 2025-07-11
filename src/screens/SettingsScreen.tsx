d4ee60042316ad1205a95fbcdc68438a4f84a98c
Mozart <dcdigitalpiano@gmail.com>
2025-07-11 14:33:17 +0900

    Update SettingsScreen.tsx

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -188,7 +188,7 @@ export default function SettingScreen({ theme, handleThemeChange }: SettingsScre
             <button onClick={handleAvatarDelete} style={{ width: 77, height: 40, background: "var(--button-secondary-bg)", border: "none", borderRadius: 8, color: "var(--text-primary)", fontSize: 14, cursor: "pointer", ...commonFontStyle }}>삭제</button>
           </div>
         </div>
-        <div style={{ width: "100%", textAlign: "center", fontSize: 14, marginTop: 26, ...commonFontStyle }}>닉네임 입력</div>
+        <div style={{ width: "100%", textAlign: "center", fontSize: 14, marginTop: 40, ...commonFontStyle }}>닉네임 입력</div>
         <input value={nickname} onChange={handleNicknameChange} style={{ width: "95%", maxWidth: "100%", height: 40, padding: "8px 16px", borderRadius: 8, border: "var(--border-light)", background: "transparent", color: "var(--text-primary)", fontSize: 16, marginTop: 15, marginBottom: 16, boxSizing: "border-box", ...commonFontStyle }} />
       </div>
 
