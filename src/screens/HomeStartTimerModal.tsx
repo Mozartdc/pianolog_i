de7f981c56723f71c9578c24ff53aa2b03d6910a
Mozart <dcdigitalpiano@gmail.com>
2025-07-16 18:29:42 +0900

    Update HomeStartTimerModal.tsx

--- src/screens/HomeStartTimerModal.tsx
+++ src/screens/HomeStartTimerModal.tsx
@@ -39,7 +39,7 @@ export function HomeStartTimerModal({
       display: "flex",
       justifyContent: "center",
       alignItems: "flex-end",
-      paddingBottom: 100,
+      paddingBottom: 120,
       zIndex: 100,
       pointerEvents: "none"
     }}>
