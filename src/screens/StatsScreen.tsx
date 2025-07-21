6cab5df736eecd64eed2adf13bb5e91327395ce7
Mozart <dcdigitalpiano@gmail.com>
2025-07-21 12:47:45 +0900

    Update StatsScreen.tsx

--- src/screens/StatsScreen.tsx
+++ src/screens/StatsScreen.tsx
@@ -356,7 +356,7 @@ const generateCalendarDays = () => {
   color: (dayIndex === 6 || isHoliday ? "var(--VIVA_MAGENTA)" : "var(--text-primary)"),
   fontWeight: isToday ? 700 : 200, // ✅ 오늘만 굵게, 나머지는 보통
   zIndex: 2, position: "relative",
-  border: isSelected ? "3px solid var(--VIVA_MAGENTA)" : "none",
+  border: isSelected ? "1px solid var(--BLACK)" : "none",
   background: "transparent",
   borderRadius: "50%",
   width: isToday || isSelected ? "100%" : "auto",
