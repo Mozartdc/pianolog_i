9cf913c68921d2378bec444a61413d85a8e944b5
Mozart <dcdigitalpiano@gmail.com>
2025-07-24 14:58:15 +0900

    Update StatsScreen.tsx

--- src/screens/StatsScreen.tsx
+++ src/screens/StatsScreen.tsx
@@ -353,7 +353,7 @@ const generateCalendarDays = () => {
                   {practiced && <DoIcon style={{ color: "var(--MIMOSA)", width: "100%", height: "100%", maxWidth: 32, maxHeight: 32, position: "absolute", zIndex: 1, opacity: 0.8 }} />}
 <span style={{
   fontSize: "clamp(12px, 4vw, 16px)",
-  color: (dayIndex === 6 || isHoliday ? "var(--VIVA_MAGENTA)" : "var(--text-primary)"),
+  color: (dayIndex === 6 || isHoliday ? "var(--VIVA_MAGENTA)" : "var(--text-secondary)"),
   fontWeight: isToday ? 700 : 200, // ✅ 오늘만 굵게, 나머지는 보통
   zIndex: 2, position: "relative",
   border: isSelected ? "1px solid var(--BLACK)" : "none",
